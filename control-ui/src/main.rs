a3680ca185382d1dcaf847d2e407aa35efeb6181
Spencer <bowles7@purdue.edu>
2022-09-12 17:01:59 -0400

    T

--- control-ui/src/main.rs
+++ control-ui/src/main.rs
@@ -1,3 +1,62 @@
-fn main() {
-    println!("Hello, world!");
+/*
+ORBITAL GNC CONTROL UI
+
+Function: Provide a visual UI for controlling the GNC Zephyr system.
+
+User Input --
+    Desired commands for Zephyr to Execute
+
+Other Input --
+    Sensor data from IMU (?)
+
+UI Output --
+    Currently running command
+    Sensor data (?)
+
+Other Output --
+    Command for rocket to run (through terminal?)
+*/
+
+use std::{io, thread, time::Duration};
+use tui::{
+    backend::CrosstermBackend,
+    widgets::{Widget, Block, Borders},
+    layout::{Layout, Constraint, Direction},
+    Terminal
+};
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+
+
+fn main() -> Result<(), io::Error> {
+    // setup terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    terminal.draw(|f| {
+        let size = f.size();
+        let block = Block::default()
+            .title("Block")
+            .borders(Borders::ALL);
+        f.render_widget(block, size);
+    })?;
+
+    thread::sleep(Duration::from_millis(5000));
+
+    // restore terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    Ok(())
 }
