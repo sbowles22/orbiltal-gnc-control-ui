e93492815b0d7f943a13a50b65a0645113999045
Spencer <bowles7@purdue.edu>
2022-09-19 17:11:58 -0400

    Create structure

---
+++ control-ui/src/lib.rs
@@ -0,0 +1,23 @@
+pub fn start_ui(app: Rc<RefCell<App>>) -> Result<()> {
+    // Configure Crossterm backend for tui
+    let stdout = stdout();
+    crossterm::terminal::enable_raw_mode()?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    terminal.clear()?;
+    terminal.hide_cursor()?;
+
+    loop {
+        let app = app.borrow();
+        // Render
+        terminal.draw(|rect| ui::draw(rect, &app))?;
+        // TODO handle inputs here
+    }
+
+    // Restore the terminal and close application
+    terminal.clear()?;
+    terminal.show_cursor()?;
+    crossterm::terminal::disable_raw_mode()?;
+
+    Ok(())
+}
--- control-ui/src/main.rs
+++ control-ui/src/main.rs
@@ -17,46 +17,62 @@ Other Output --
     Command for rocket to run (through terminal?)
 */
 
-use std::{io, thread, time::Duration};
-use tui::{
-    backend::CrosstermBackend,
-    widgets::{Widget, Block, Borders},
-    layout::{Layout, Constraint, Direction},
-    Terminal
-};
-use crossterm::{
-    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
-    execute,
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-};
+// use std::{io, thread, time::Duration};
+// use tui::{
+//     backend::CrosstermBackend,
+//     widgets::{Widget, Block, Borders},
+//     layout::{Layout, Constraint, Direction},
+//     Terminal
+// };
+//
+// use crossterm::{
+//     event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+//     execute,
+//     terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+// };
+//
+// use std::net::{TcpListener, TcpStream, Shutdown};
+// use std::io::{Read, Write};
+//
+// fn handle_client(mut stream: TcpStream) {
+//     let mut data = [0 as u8; 50]; // using 50 byte buffer
+//     while match stream.read(&mut data) {
+//         Ok(size) => {
+//             // echo everything!
+//             stream.write(&data[0..size]).unwrap();
+//             true
+//         },
+//         Err(_) => {
+//             println!("An error occurred, terminating connection with {}", stream.peer_addr().unwrap());
+//             stream.shutdown(Shutdown::Both).unwrap();
+//             false
+//         }
+//     } {}
+// }
 
-
-fn main() -> Result<(), io::Error> {
-    // setup terminal
-    enable_raw_mode()?;
-    let mut stdout = io::stdout();
-    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
-    let backend = CrosstermBackend::new(stdout);
-    let mut terminal = Terminal::new(backend)?;
-
-    terminal.draw(|f| {
-        let size = f.size();
-        let block = Block::default()
-            .title("Block")
-            .borders(Borders::ALL);
-        f.render_widget(block, size);
-    })?;
-
-    thread::sleep(Duration::from_millis(5000));
-
-    // restore terminal
-    disable_raw_mode()?;
-    execute!(
-        terminal.backend_mut(),
-        LeaveAlternateScreen,
-        DisableMouseCapture
-    )?;
-    terminal.show_cursor()?;
-
+fn main() -> Result<()> {
+    let app = Rc::new(RefCell::new(App::new())); // TODO app is useless for now
+    start_ui(app)?;
     Ok(())
 }
+
+// let listener = TcpListener::bind("0.0.0.0:65432").unwrap();
+// // accept connections and process them, spawning a new thread for each one
+// println!("Server listening on port 65432");
+// for stream in listener.incoming() {
+//     match stream {
+//         Ok(stream) => {
+//             println!("New connection: {}", stream.peer_addr().unwrap());
+//             thread::spawn(move|| {
+//                 // connection succeeded
+//                 handle_client(stream)
+//             });
+//         }
+//         Err(e) => {
+//             println!("Error: {}", e);
+//             /* connection failed */
+//         }
+//     }
+// }
+// // close the socket server
+// drop(listener);
